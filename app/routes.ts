60300ec7b771e6473cf1055220950c74217e5657
Henrik Reusch <henrikreusch@gmail.com>
2025-04-26 17:55:56 +0200

    VIDEO player on home screen

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("/hjelp" ,"routes/help.tsx"),
-  route("/hard", "routes/hard.tsx"),
   route("/more-info", "routes/more-info.tsx"),
   route("/see-ui-elements", "routes/see-ui-elements.tsx"),
 ] satisfies RouteConfig;
